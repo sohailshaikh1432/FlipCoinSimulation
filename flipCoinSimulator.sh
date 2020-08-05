41704c856da429ea3b0afb3b813753cbbe1ab81d
sohailshaikh1432 <sohailshaikh1432.ss@gmail.com>
2020-08-06 03:31:02 +0530

    Flip a multiple coin to Display Head or Tail as Winner

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -x
 
-coinFlip=$((RANDOM%2))
-if [ $coinFlip -eq 0 ]
-then
-	echo "Heads is winner"
-else
-	echo "Tails is winner"
-fi
+numberOfHeads=0
+numberOfTails=0
+for (( i=0 ; i<10 ; i++ ))
+do
+	coinFlip=$((RANDOM%2))
+	if [ $coinFlip -eq 0 ]
+	then
+		((numberOfHeads++))
+	else
+		((numberOfTails++))
+	fi
+done
+echo "Heads has won $numberOfHeads times and tails has won $numberOfTails times"
