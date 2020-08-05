786d934a3103020ba806e2ad4acf273f790e0344
sohailshaikh1432 <sohailshaikh1432.ss@gmail.com>
2020-08-06 03:40:35 +0530

    Flip a coin continue till tjhe difference of minimum 2 point is achived.

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,26 +2,55 @@
 
 numberOfHeads=0
 numberOfTails=0
-for (( i=0 ; ; i++ ))
+function coinFlip(){
+coinFlip=$((RANDOM%2))
+if [ $coinFlip -eq 0 ]
+then
+	((numberOfHeads++))
+else
+	((numberOfTails++))
+fi
+}
+function tie(){
+for (( j=0 ; ; j++ ))
 do
-	coinFlip=$((RANDOM%2))
-	if [ $coinFlip -eq 0 ]
+	coinFlip
+	if [ $(($numberOfHeads-$numberOfTails)) -ge 2 ]
 	then
-		((numberOfHeads++))
-	else
-		((numberOfTails++))
-	fi
-	if [ $numberOfHeads -eq 21 ]
-	then
-		echo "Heads win by $(($numberOfHeads-$numberOfTails))"
+		echo "Heads win"
 		break
-	elif [ $numberOfTails -eq 21 ]
+	elif [ $(($numberOfTails-$numberOfHeads)) -ge 2 ]
 	then
-		echo "Tails win by $(($numberOfTails-$numberOfHeads))"
-		break
-	elif [ $numberOfHeads -eq 20 ] && [ $numberOfTails -eq 20 ]
-	then
-		echo "Tie"
+		echo "Tails win"
 		break
 	fi
 done
+}
+for (( i=0 ; ; i++ ))
+do
+	coinFlip
+	if [ $numberOfHeads -eq 21 ]
+	then
+		outcome=0
+		break
+	elif [ $numberOfTails -eq 21 ]
+	then
+		outcome=1
+		break
+	elif [ $numberOfHeads -eq 20 ] && [ $numberOfTails -eq 20 ]
+	then
+		outcome=2
+		break
+	fi
+done
+if [ $outcome -eq 2 ]
+then
+	outcomeAfterTie="$(tie)"
+	echo $outcomeAfterTie
+elif [ $outcome -eq 1 ]
+then
+	echo "Tails win by $(($numberOfTails-$numberOfHeads))"
+elif [ $outcome -eq 0 ]
+then
+	echo "Heads win by $(($numberOfHeads-$numberOfTails))"
+fi
