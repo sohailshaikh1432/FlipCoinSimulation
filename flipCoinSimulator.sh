2554c4b5282dd98a2a15acbfc9790da8d7e492fc
sohailshaikh1432 <sohailshaikh1432.ss@gmail.com>
2020-08-06 03:36:30 +0530

    Flip a coin continue till either of them have won 21 times.

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,7 +2,7 @@
 
 numberOfHeads=0
 numberOfTails=0
-for (( i=0 ; i<10 ; i++ ))
+for (( i=0 ; ; i++ ))
 do
 	coinFlip=$((RANDOM%2))
 	if [ $coinFlip -eq 0 ]
@@ -11,5 +11,17 @@ do
 	else
 		((numberOfTails++))
 	fi
+	if [ $numberOfHeads -eq 21 ]
+	then
+		echo "Heads win by $(($numberOfHeads-$numberOfTails))"
+		break
+	elif [ $numberOfTails -eq 21 ]
+	then
+		echo "Tails win by $(($numberOfTails-$numberOfHeads))"
+		break
+	elif [ $numberOfHeads -eq 20 ] && [ $numberOfTails -eq 20 ]
+	then
+		echo "Tie"
+		break
+	fi
 done
-echo "Heads has won $numberOfHeads times and tails has won $numberOfTails times"
